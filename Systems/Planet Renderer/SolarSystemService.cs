56e2ebcca3244ffd7e68f12f520cbf4a3634910a
Oznogon <garrett@oznogon.com>
2018-02-08 02:55:44 -0800

    Adjust indents.

--- Systems/Planet Renderer/SolarSystemService.cs
+++ Systems/Planet Renderer/SolarSystemService.cs
@@ -2,32 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Spaceworks.Position;
+
 namespace Spaceworks { 
 
-	public class SolarSystemService : MonoBehaviour {
+    public class SolarSystemService : FloatingTransform {
 
-		public SolarSystemConfig config;
+        public SolarSystemConfig config;
 
-		public SolarSystem generatedSystem { get; private set;}
-		public Planet[] generatedPlanets { get; private set;}
+        public SolarSystem generatedSystem { get; private set;}
+        public Planet[] generatedPlanets { get; private set;}
 
-		void Start(){
-			SolarSystem s = new SolarSystem (config);
-			generatedPlanets = s.RenderOn (this.gameObject);
-			generatedSystem = s;
+        void Start(){
+            SolarSystem s = new SolarSystem (config);
+            generatedPlanets = s.RenderOn (this.gameObject);
+            generatedSystem = s;
 
             foreach(Planet p in generatedPlanets) {
                 p.ForceUpdateLODs(Camera.main.transform.position);
             }
 
             this.name = this.name + ": " + config.name;
-		}
+        }
 
-		void Update(){
+        void Update(){
             foreach (Planet p in generatedPlanets) {
                 p.UpdateLODs(Camera.main.transform.position);
             }
         }
-	}
 
+        public override void OnOriginChange(WorldPosition sceneCenter, WorldPosition delta = null)
+        {
+            base.OnOriginChange(sceneCenter);
+            foreach (Planet p in generatedPlanets)
+            {
+                if (p != null)
+                    p.UpdateMaterial(p.topFace.transform.gameObject);
+            }
+        }
+    }
 }
